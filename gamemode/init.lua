9b0187e8812e9c13da8dfed89301f7510bf4d474
MisterTakaashi <soapmctravich@gmail.com>
2014-08-31 16:44:02 +0200

    Changement de skin en fonction de la team - Skins non definitifs

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -4,5 +4,9 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 function GM:PlayerSetModel(ply)
-    ply:SetModel("models/player/odessa.mdl")
+    if (ply:Team() == 1) then
+        ply:SetModel("models/player/odessa.mdl")
+    else
+        ply:SetModel("models/player/breen.mdl")
+    end
 end
