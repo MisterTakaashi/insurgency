230638f2cc9e5cc26c958b1ddebc3a52d498fd33
MisterTakaashi <soapmctravich@gmail.com>
2014-08-31 16:13:07 +0200

    Ajout team

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -3,6 +3,8 @@ GM.Author = "MisterTakaashi"
 GM.Email = "mistertakaashi@fancysoftware.net"
 GM.Website = "http://www.fancysoftware.net"
 
+include("team.lua")
+
 function GM:Initialize()
 	-- Do stuff
 end
