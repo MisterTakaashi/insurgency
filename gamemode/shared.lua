8605b43ca19e6326db2ee6646e85aad2a36b50f6
MisterTakaashi <soapmctravich@gmail.com>
2014-08-31 05:25:21 +0200

    Ajout fichiers base

---
+++ gamemode/shared.lua
@@ -0,0 +1,8 @@
+GM.Name = "Insurgency"
+GM.Author = "MisterTakaashi"
+GM.Email = "mistertakaashi@fancysoftware.net"
+GM.Website = "http://www.fancysoftware.net"
+
+function GM:Initialize()
+	-- Do stuff
+end
