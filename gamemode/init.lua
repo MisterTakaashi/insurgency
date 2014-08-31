70bf2e84588783fa251194dc828ab36c6215c170
MisterTakaashi <soapmctravich@gmail.com>
2014-08-31 16:57:25 +0200

    Ajout message et arme au spawn pour test

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -3,6 +3,11 @@ AddCSLuaFile("shared.lua")
 
 include("shared.lua")
 
+function GM:PlayerSpawn( ply )
+    MsgN(ply:Nick() .. " vient de rejoindre la bataille !" )
+    ply:Give("weapon_physgun")
+end
+
 function GM:PlayerSetModel(ply)
     if (ply:Team() == 1) then
         ply:SetModel("models/player/odessa.mdl")
