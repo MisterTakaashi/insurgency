9341c8619ea515a55007d6f0e002f19295d307fd
MisterTakaashi <soapmctravich@gmail.com>
2014-08-31 20:07:15 +0200

    Ajout crosshair caché

--- gamemode/cl_hud.lua
+++ gamemode/cl_hud.lua
@@ -1,5 +1,5 @@
 local function hideElements(name)
-	if name == "CHudHealth" or name == "CHudBattery" or name == "CHudSuitPower" then
+	if name == "CHudHealth" or name == "CHudBattery" or name == "CHudSuitPower" or name == "CHudCrosshair" then
 		return false
 	end
 end
