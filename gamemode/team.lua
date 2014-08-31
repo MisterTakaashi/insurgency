198b388a07a056bd8454a8b5c25621c1e1b6efb2
root <root@localhost.localdomain>
2014-08-31 16:11:39 +0200

    Ajout team

---
+++ gamemode/team.lua
@@ -0,0 +1,13 @@
+// Deux equipes de bases > 1 & 2
+
+team.SetUp(1, "Insurgent", Color(255, 0, 0))
+team.SetUp(2, "Security", Color(0, 0, 255))
+
+// Classes suivant les equipes
+// Classes des insurges
+
+team.Setup(11, "Soldat", Color(255, 0, 0))
+
+// Classes des security
+
+team.SetUp(21, "Agent", Color(0, 0, 255))
